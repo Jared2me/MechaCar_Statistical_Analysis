add53c154bb769752057475ca67316cd21710b1a
Jared2me <Jaredtoomey@icloud.com>
2022-10-03 18:01:21 -0400

    Update

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,17 @@
 library(dplyr)
 library(jsonlite)
+library(tidyverse)
+
+MechaMPG <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+head(MechaMPG)
+
+MechaMPG_matrix <- as.matrix(MechaMPG)
+
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaMPG)
+MechaMPG_sum <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaMPG)
+summary(MechaMPG_sum)
+
+summary(MechaMPG)
+summary(MechaMPG_matrix)
 
 
