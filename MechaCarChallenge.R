f4cee74d667012addb8f63a639aa30e5421ab12e
Jared2me <Jaredtoomey@icloud.com>
2022-10-03 20:03:41 -0400

    Update

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(jsonlite)
 library(tidyverse)
 
+#D1
 MechaMPG <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 head(MechaMPG)
 
@@ -9,9 +10,31 @@ MechaMPG_matrix <- as.matrix(MechaMPG)
 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaMPG)
 MechaMPG_sum <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaMPG)
-summary(MechaMPG_sum)
 
 summary(MechaMPG)
 summary(MechaMPG_matrix)
+summary(MechaMPG_sum)
 
+
+#D2
+MechaCoil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+total_summary <- summarise(MechaCoil, Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+lots <- group_by(MechaCoil, Manufacturing_Lot)
+lot_summary <- summarize(lots, Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+
+#D3
+t.test(MechaCoil$PSI, mu=1500)
+?t.test
+?subset
+
+lot1 <- subset(MechaCoil, Manufacturing_Lot=='Lot1')
+t.test(lot1$PSI, mu=1500)
+
+lot2 <- subset(MechaCoil, Manufacturing_Lot=='Lot2')
+t.test(lot2$PSI, mu=1500)
+
+lot3 <- subset(MechaCoil, Manufacturing_Lot=='Lot3')
+t.test(lot3$PSI, mu=1500)
 
